b0f2dce649ad7022479dbb281eff0878f24358fd
Ravil <star.toni@mail.ru>
2023-12-08 18:59:28 +0600

    [RO] Adding UI

--- Robinhoods Options/AliasResultViewController.swift
+++ Robinhoods Options/AliasResultViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 class AliasResultViewController: UIViewController {
 
     let resultBackground = UIImageView()
+    let cardTeam = UIView()
+    let titleGuessed = UILabel()
+    let amountGuessed = UILabel()
+    let titleSkipped = UILabel()
+    let amountSkipped = UILabel()
+    let buttonNextTeam = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationController?.setNavigationBarHidden(true, animated: false)
         setupUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        cardTeam.layer.cornerRadius = 30
     }
     
     private func setupUI() {
@@ -24,11 +37,74 @@ class AliasResultViewController: UIViewController {
         resultBackground.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(resultBackground)
         
+        cardTeam.backgroundColor = .white
+        cardTeam.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cardTeam)
+        
+        titleGuessed.text = "Guessed"
+        titleGuessed.textColor = .black
+        titleGuessed.numberOfLines = 0
+        titleGuessed.textAlignment = .center
+        titleGuessed.font = UIFont(name: "NotoSans-SemiBold", size: 32)
+        titleGuessed.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleGuessed)
+        
+        amountGuessed.text = "10"
+        amountGuessed.textColor = .black
+        amountGuessed.numberOfLines = 0
+        amountGuessed.textAlignment = .center
+        amountGuessed.font = UIFont(name: "NotoSans-SemiBold", size: 32)
+        amountGuessed.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(amountGuessed)
+        
+        titleSkipped.text = "Skipped"
+        titleSkipped.textColor = .black
+        titleSkipped.numberOfLines = 0
+        titleSkipped.alpha = 0.50
+        titleSkipped.textAlignment = .center
+        titleSkipped.font = UIFont(name: "NotoSans-SemiBold", size: 16)
+        titleSkipped.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleSkipped)
+        
+        amountSkipped.text = "5"
+        amountSkipped.textColor = .black
+        amountSkipped.numberOfLines = 0
+        amountSkipped.alpha = 0.50
+        amountSkipped.textAlignment = .center
+        amountSkipped.font = UIFont(name: "NotoSans-SemiBold", size: 16)
+        amountSkipped.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(amountSkipped)
+        
+        buttonNextTeam.setImage(UIImage(named: "nextTeam"), for: .normal)
+        buttonNextTeam.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonNextTeam)
+        
         NSLayoutConstraint.activate([
             resultBackground.topAnchor.constraint(equalTo: view.topAnchor),
             resultBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             resultBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             resultBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            cardTeam.topAnchor.constraint(equalTo: view.topAnchor, constant: 138),
+            cardTeam.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 47),
+            cardTeam.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -47),
+            cardTeam.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardTeam.heightAnchor.constraint(equalToConstant: 96),
+            
+            titleGuessed.topAnchor.constraint(equalTo: cardTeam.bottomAnchor, constant: 122),
+            titleGuessed.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            amountGuessed.topAnchor.constraint(equalTo: cardTeam.bottomAnchor, constant: 122),
+            amountGuessed.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            titleSkipped.topAnchor.constraint(equalTo: titleGuessed.bottomAnchor, constant: 8),
+            titleSkipped.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            amountSkipped.topAnchor.constraint(equalTo: amountGuessed.bottomAnchor, constant: 8),
+            amountSkipped.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            buttonNextTeam.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            buttonNextTeam.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
 }
--- Robinhoods Options/SceneDelegate.swift
+++ Robinhoods Options/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         window.overrideUserInterfaceStyle = .light
         window.rootViewController =
-        UINavigationController(rootViewController: AliasTeamViewController())
+        UINavigationController(rootViewController: AliasResultViewController())
         window.makeKeyAndVisible()
         self.window = window
     }
